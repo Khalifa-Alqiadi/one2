e5c26152c33eb27e76bf694cd58a4987892d1851
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2026-04-13 02:03:19 +0300

    test

--- core/resources/views/frontEnd/layouts/footer.blade.php
+++ core/resources/views/frontEnd/layouts/footer.blade.php
@@ -20,7 +20,7 @@ if (!Helper::GeneralSiteSettings("style_subscribe")) {
                 <div class="row">
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="footer-logo mb-1 text-center">
-                            <img alt="{{ Helper::GeneralSiteSettings("site_title_" . @Helper::currentLanguage()->code) }}" src="{{ route("fileView",["path" =>'settings/logo-website.png' ]) }}" class="">
+                            <img alt="{{ Helper::GeneralSiteSettings("site_title_" . @Helper::currentLanguage()->code) }}" src="{{ URL::to('uploads/settings/logo-website.png') }}" class="">
                         </div>
                         @include("frontEnd.layouts.social",["tt_position"=>"top"])
                     </div>
