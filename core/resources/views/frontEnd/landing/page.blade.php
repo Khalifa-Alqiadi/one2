ee05020386a1037f5e1ed63faedc1f300efc451b
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2026-04-25 22:50:53 +0300

    test

--- core/resources/views/frontEnd/landing/page.blade.php
+++ core/resources/views/frontEnd/landing/page.blade.php
@@ -26,8 +26,8 @@
     <div class="landing-page">
         @include('frontEnd.homepage.ads-top')
         @include('frontEnd.homepage.teams')
-        @include('frontEnd.homepage.leagues')
         @include('frontEnd.homepage.matches')
+        @include('frontEnd.homepage.leagues')
 
         @if($Topic->topicBlocks->where("status",1)->count() >0)
             @foreach($Topic->topicBlocks->where("status",1) as $TopicBlock)
