f86e9431ecf7ee0304c3ad928460774e461680b9
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2026-04-26 23:45:47 +0300

    test

--- core/resources/views/frontEnd/layouts/head.blade.php
+++ core/resources/views/frontEnd/layouts/head.blade.php
@@ -22,12 +22,12 @@
 <link rel="stylesheet" href="{{ URL::asset('assets/frontend/vendor/owl-carousel/assets/owl.theme.default.min.css') }}?v={{ Helper::system_version() }}">
 
 <link href="{{ URL::asset('assets/frontend/css/style.css') }}?v=2" rel="stylesheet"/>
-<link href="{{ URL::asset('assets/frontend/css/custom.css') }}?v=86" rel="stylesheet"/>
-<link href="{{ URL::asset('assets/frontend/css/matches.css') }}?v=20" rel="stylesheet"/>
+<link href="{{ URL::asset('assets/frontend/css/custom.css') }}?v=87" rel="stylesheet"/>
+<link href="{{ URL::asset('assets/frontend/css/matches.css') }}?v=21" rel="stylesheet"/>
 
 
 @if( @Helper::currentLanguage()->direction=="rtl")
-<link href="{{ URL::asset('assets/frontend/css/rtl.css') }}?v=17" rel="stylesheet"/>
+<link href="{{ URL::asset('assets/frontend/css/rtl.css') }}?v=18" rel="stylesheet"/>
 @endif
 
 <!-- Favicon and Touch Icons -->
